0fdc538a2af79bda09e6297bc7d027939bd4a2ed
Ibraheam Alkhalifah <hi@ibraheam.dev>
2022-12-20 00:38:14 +0300

    disable background

--- lua/init-theme.lua
+++ lua/init-theme.lua
@@ -21,7 +21,7 @@ require('solarized').set()
 -- use local function
 vim.g.nord_contrast = true
 vim.g.nord_borders = true
-vim.g.nord_disable_background = false 
+vim.g.nord_disable_background = true 
 vim.g.nord_italic = false
 vim.g.nord_uniform_diff_background = true
 require('nord').set()
