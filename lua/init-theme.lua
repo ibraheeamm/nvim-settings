18274a2ef57d85e73c4a0855d11dc610f2dfcfc6
ibraheam <ibraheam@ibraheams-MacBook-Pro.local>
2022-12-21 00:50:53 +0300

    update

--- lua/init-theme.lua
+++ lua/init-theme.lua
@@ -1,23 +1,4 @@
--- Example config in lua
 
--- if morning change theme to solarizd if night to nord in lua
-
--- time in lua
-
-
-
---[=[
-vim.g.solarized_italic_comments = true
-vim.g.solarized_italic_keywords = true
-vim.g.solarized_italic_functions = true
-vim.g.solarized_italic_variables = false
-vim.g.solarized_contrast = true
-vim.g.solarized_borders = false
-vim.g.solarized_disable_background = false
--- Load the colorscheme
-require('solarized').set()
---]=]
--- time in lua
 -- use local function
 vim.g.nord_contrast = true
 vim.g.nord_borders = true
