39cd58940d71742fff2799038763d81ff6c65e6a
Ibrahim Alkhalifah <hi@ibraheam.dev>
2024-03-19 00:14:22 +0300

    update

--- lua/init-null-ls.lua
+++ lua/init-null-ls.lua
@@ -2,8 +2,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
---        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
-        null_ls.builtins.code_actions.shellcheck,
     },
 })
